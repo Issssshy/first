1898571b480021c74ea42b1781f77b3d7d302603
Issssshy <ishieyuma@gmail.com>
2024-02-15 11:52:01 +0900

    9-3

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Blog</title>
+<!--<!DOCTYPE html>-->
+
+    <!--<head>-->
+        <!--<meta charset="utf-8">-->
+        <!--<title>Blog</title>-->
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
+        <!--<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">-->
+    <!--</head>-->
+    <x-app-layout>
+        <x-slot name="header">
+            index
+        </x-slot>
     <body>
         <h1>Blog Name</h1>
         <a href='/posts/create'>create</a>
@@ -35,5 +39,7 @@
             }
         </script>
         </div>
+        {{ Auth::user()->name }}
     </body>
-</html>
+    </x-app-layout>
+<!--</html>-->
